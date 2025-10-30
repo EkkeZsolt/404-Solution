f3bc8e630c0d4ba969114612a48de377152570a0
WolfKraiton <144487069+WolfKraiton@users.noreply.github.com>
2025-10-30 12:56:30 +0100

    gomb átnavigálása más oldalakra

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,47 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+import {BrowserRouter as Router, Routes, Route, useNavigate} from "react-router-dom";
 import './App.scss'
 import './index.scss';
 import Hero from './hero/Hero';
-=======
-import React from 'react'
-import { useState } from 'react'
-import Quiz from './QuizApp/Quiz'
-=======
->>>>>>> 01c15ec (fix: felesleges volt beimportolni a react-et mert react 17 után nem kell kiírni)
-import './App.scss'
-<<<<<<< HEAD
-import './index.css';
->>>>>>> 82f1200 (fix: nem működik még mindig valamiért a reac, azt próbálom megcsinálni, a main.tsx.-ben rossz elérési út volt megadva az index.scss-nek)
-=======
-import './index.scss';
->>>>>>> 36c4ef1 (fix: a probléma nálam volt nem volt telepítve a sass csomag xd)
-import Hero from './hero/Hero';
+import LoginPage from './loginpages/login';
+import RegisterPage from './loginpages/register';
 
-function App() {
+function Header() {
+  const navigate = useNavigate();
+
   return (
     <>
       <header className="site-header">
         <div className="logo">L</div>
         <nav className="auth">
-          <button className="auth-btn">Bejelentkezés/Regisztráció</button>
+          <button className="auth-btn" onClick={() => navigate("/login")}>Bejelentkezés/Regisztráció</button>
         </nav>
       </header>
+    </>
+  );
+}
 
+function HomePage() {
+  return (
+    <>
+      <Header />
       <main>
         <Hero />
       </main>
     </>
   );
 }
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  )
+}
+
 
 export default App;
