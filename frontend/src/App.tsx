a6298f85fd3c55e1b6ca0522fc139e94c16db07e
WolfKraiton <144487069+WolfKraiton@users.noreply.github.com>
2025-10-30 12:56:30 +0100

    gomb átnavigálása más oldalakra

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,47 @@
+import {BrowserRouter as Router, Routes, Route, useNavigate} from "react-router-dom";
 import './App.scss'
 import './index.scss';
 import Hero from './hero/Hero';
+import LoginPage from './loginpages/login';
+import RegisterPage from './loginpages/register';
 
-function App() {
+function Header() {
+  const navigate = useNavigate();
+
   return (
     <>
       <header className="site-header">
         <div className="logo">L</div>
         <nav className="auth">
-          <button className="auth-btn">Bejelentkezés/Regisztráció</button>
+          <button className="auth-btn" onClick={() => navigate("/login")}>Bejelentkezés/Regisztráció</button>
         </nav>
       </header>
+    </>
+  );
+}
 
+function HomePage() {
+  return (
+    <>
+      <Header />
       <main>
         <Hero />
       </main>
     </>
   );
 }
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  )
+}
+
 
 export default App;
