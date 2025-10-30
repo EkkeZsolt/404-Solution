a5e0610a65ab33087918101eff3b54fa9dcd5703
WolfKraiton <144487069+WolfKraiton@users.noreply.github.com>
2025-10-30 13:00:52 +0100

    register error message update

--- frontend/src/loginpages/register.tsx
+++ frontend/src/loginpages/register.tsx
@@ -1,6 +1,37 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 import "./Register.scss";
 
 export default function Register() {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [role, setRole] = useState(""); 
+  const [error, setError] = useState("");
+  const handleRegister = () => {
+    if (!username || !email || !password || !confirmPassword || !role){
+      setError("Kérlek, tölts ki minden mezőt!");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setError("A két jelszó nem egyezik meg!");
+      return;
+    }
+
+    if (role !== "tanar" && role !== "diak"){
+      setError("Kérlek, válaszd ki, hogy tanár vagy diák vagy!");
+      return;
+    }
+
+    setError("");
+    alert("Sikeres regisztráció");
+    navigate("/login");
+  };
+  
   return (
     <div className="register-page">
       <h1>Regisztráció</h1>
@@ -15,6 +46,7 @@ export default function Register() {
             <option value="tanar">Tanár</option>
             <option value="diak">Diák</option>
           </select>
+          {error && <p className="error-message">{error}</p>}
           <button type="button">Regisztráció</button>
         </form>
       </div>
