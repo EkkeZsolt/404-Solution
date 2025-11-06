20f6818c08835d86a9f252e598242b85b696eea1
WolfKraiton <144487069+WolfKraiton@users.noreply.github.com>
2025-11-06 14:19:44 +0100

    feat: implement login with email and role-based navigation

--- frontend/src/loginpages/login.tsx
+++ frontend/src/loginpages/login.tsx
@@ -5,27 +5,54 @@ import "./Login.scss";
 export default function Login() {
   const navigate = useNavigate();
 
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleRegister = (e: React.FormEvent) => {
+  const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
-    if (!username || !password){
+    if (!email || !password){
       setError("Kérlek, tölts ki minden mezőt!");
       return;
     }
-    setError("");
-    alert("Sikeres bejelentkezés");
-    navigate("/");
-  }
+  try {
+      const response = await fetch("http://127.0.0.1:8000/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({email, password}),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        setError(data.message || "Hiba történt a bejelentkezés során.");
+        return;
+      }
+
+      const userRole = data.user.role;
+
+      localStorage.setItem("token", data.token);
+
+      if (userRole.toLowerCase() === "tanar") {
+        navigate("/tanarfelulet");
+      } else if (userRole.toLowerCase() === "diak") {
+        navigate("/diakfelulet");
+      } else {
+        setError("Ismeretlen felhasználói szerep.");
+      }
+    } catch (err) {
+      setError("Hálózati hiba, próbáld újra.");
+      console.error(err);
+    }
+  }  
+  
   
   return (
     <div className="login-page">
       <h1>Bejelentkezés</h1>
       <div className="form-wrapper">
         <form className="login-form" onSubmit={handleRegister}>
-          <input type="text" placeholder="Felhasználónév" value={username} onChange={(e) => setUsername(e.target.value)}/>
+          <input type="text" placeholder="Felhasználónév" value={email} onChange={(e) => setEmail(e.target.value)}/>
           <input type="password" placeholder="Jelszó" value={password} onChange={(e) => setPassword(e.target.value)}/>
           {error && <p className="error-message">{error}</p>}
           <button type="submit">Login</button>
