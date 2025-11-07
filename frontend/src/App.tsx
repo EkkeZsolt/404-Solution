e12f57489320d9f6df64797238d65c3bb5254629
EkkeZsolt <ekkezsolt@gmail.com>
2025-11-07 22:32:16 +0100

    fix: nem működik még mindig valamiért a reac, azt próbálom megcsinálni, a main.tsx.-ben rossz elérési út volt megadva az index.scss-nek

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
+<<<<<<< HEAD
+import './App.scss'
+import './index.scss';
+import Hero from './hero/Hero';
+=======
 import React from 'react'
 import { useState } from 'react'
 import Quiz from './QuizApp/Quiz'
 import './App.scss'
 import Hero from './components/Hero';
 import './index.css';
+>>>>>>> 82f1200 (fix: nem működik még mindig valamiért a reac, azt próbálom megcsinálni, a main.tsx.-ben rossz elérési út volt megadva az index.scss-nek)
 
 function App() {
   return (
