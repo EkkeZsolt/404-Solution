b4ade311678b90cc8184208e922d9bd71f46fab5
EkkeZsolt <ekkezsolt@gmail.com>
2025-11-07 22:32:28 +0100

    fix: a probléma nálam volt nem volt telepítve a sass csomag xd mostmár minden faja lesz💡

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import './App.scss'
 import './index.scss';
 import Hero from './hero/Hero';
@@ -6,10 +7,16 @@ import Hero from './hero/Hero';
 import React from 'react'
 import { useState } from 'react'
 import Quiz from './QuizApp/Quiz'
+=======
+>>>>>>> 01c15ec (fix: felesleges volt beimportolni a react-et mert react 17 után nem kell kiírni)
 import './App.scss'
-import Hero from './components/Hero';
+<<<<<<< HEAD
 import './index.css';
 >>>>>>> 82f1200 (fix: nem működik még mindig valamiért a reac, azt próbálom megcsinálni, a main.tsx.-ben rossz elérési út volt megadva az index.scss-nek)
+=======
+import './index.scss';
+>>>>>>> 36c4ef1 (fix: a probléma nálam volt nem volt telepítve a sass csomag xd)
+import Hero from './hero/Hero';
 
 function App() {
   return (
