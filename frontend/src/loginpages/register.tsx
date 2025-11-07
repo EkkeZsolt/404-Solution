be9623199eed01911c12b65b394547bff80add0b
EkkeZsolt <ekkezsolt@gmail.com>
2025-11-07 22:31:38 +0100

    Create register.tsx

--- frontend/src/loginpages/register.tsx
+++ frontend/src/loginpages/register.tsx
@@ -1,103 +1,21 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a5e0610 (register error message update)
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
 import "./Register.scss";
 
 export default function Register() {
-  const navigate = useNavigate();
-
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [role, setRole] = useState(""); 
-  const [error, setError] = useState("");
-<<<<<<< HEAD
-<<<<<<< HEAD
-  const handleRegister = (e: React.FormEvent) => {
-    e.preventDefault();
-<<<<<<< HEAD
-=======
-  const handleRegister = () => {
->>>>>>> a5e0610 (register error message update)
-=======
-  const handleRegister = (e: React.FormEvent) => {
->>>>>>> dc6b96f (register javítása)
-=======
->>>>>>> 3f8a81a (hiányzó sor berakása)
-    if (!username || !email || !password || !confirmPassword || !role){
-      setError("Kérlek, tölts ki minden mezőt!");
-      return;
-    }
-
-    if (password !== confirmPassword) {
-      setError("A két jelszó nem egyezik meg!");
-      return;
-    }
-
-    if (role !== "tanar" && role !== "diak"){
-      setError("Kérlek, válaszd ki, hogy tanár vagy diák vagy!");
-      return;
-    }
-
-    setError("");
-    alert("Sikeres regisztráció");
-    navigate("/login");
-  };
-  
-<<<<<<< HEAD
-=======
-import "./Register.scss";
-
-export default function Register() {
->>>>>>> b7e6022 (Create register.tsx)
-=======
->>>>>>> a5e0610 (register error message update)
   return (
     <div className="register-page">
       <h1>Regisztráció</h1>
       <div className="form-wrapper">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> dc6b96f (register javítása)
-        <form className="register-form" onSubmit={handleRegister}> 
-          <input type="text" placeholder="Felhasználónév" value={username} onChange={(e) => setUsername(e.target.value)}/>
-          <input type="password" placeholder="Jelszó" value={password} onChange={(e) => setPassword(e.target.value)}/>
-          <input type="password" placeholder="Jelszó újra" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
-          <input type="email" placeholder="Email cím" value={email} onChange={(e) => setEmail(e.target.value)}/>
-          <select value={role} onChange={(e) => setRole(e.target.value)}>
-<<<<<<< HEAD
-=======
         <form className="register-form">
           <input type="text" placeholder="Felhasználónév" />
           <input type="password" placeholder="Jelszó" />
           <input type="password" placeholder="Jelszó újra" />
           <input type="email" placeholder="Email cím" />
           <select>
->>>>>>> b7e6022 (Create register.tsx)
-=======
->>>>>>> dc6b96f (register javítása)
             <option value="">Tanár / Diák</option>
             <option value="tanar">Tanár</option>
             <option value="diak">Diák</option>
           </select>
-<<<<<<< HEAD
-<<<<<<< HEAD
-          {error && <p className="error-message">{error}</p>}
-          <button type="submit">Regisztráció</button>
-<<<<<<< HEAD
-=======
-=======
-          {error && <p className="error-message">{error}</p>}
->>>>>>> a5e0610 (register error message update)
           <button type="button">Regisztráció</button>
->>>>>>> b7e6022 (Create register.tsx)
-=======
->>>>>>> 04a01e9 (register.tsx javítás)
         </form>
       </div>
     </div>
